a02d9d03df6eae8a9db5b48ed5321c42d755b882
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2021-10-13 16:59:07 -0600

    randomly generate amazon banner

--- public/index.js
+++ public/index.js
@@ -13,3 +13,28 @@ $(document).ready(function() {
       localStorage.setItem('switch-state', el.hasClass('dark-mode'));
     });
   });
+
+
+
+function getRandomUrl(urls) {
+    var minIndex = 0;
+    var maxIndex = urls.length;
+    var randomIndex = Math.floor(Math.random() * (maxIndex - minIndex)) + minIndex;
+    return urls[randomIndex];
+}
+
+var urls = [
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=kindleunlimited&banner=1CNN295Y0KRG45FHJ0R2&f=ifr&linkID=c4227d2057eeadf376b4a8ef09eeb669&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=amazon_homepage&banner=0KN4RVW6K8W9PXK9RJR2&f=ifr&linkID=67275aff26ddcbfd1f4d808b84dbb824&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=amu&banner=1DD75S12TA4MJE6ZDYR2&f=ifr&linkID=0b4d5b433555dd23d27671defcd163fc&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=back2business&banner=0H7Q40GA45GNWQ31PW82&f=ifr&linkID=b313873039035e5d3e5b99f037e031ab&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=books&banner=1CF5A619J93FZ5Z2EQ82&f=ifr&linkID=f82e4599e9f83285c29ed84d361e2fe9&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=moviesandtv&banner=12CP6TJX1FMF2YX69QR2&f=ifr&linkID=4e75e0ffaa22f56301d15f9d6974a60e&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=primestudent&banner=09T4RTRGDTMCPCMYJVR2&f=ifr&linkID=b8e5d36f4c8a8caea6d510af850a7720&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=48&l=ur1&category=sportsoutdoors&banner=06FDBQSTRYXP74Q959G2&f=ifr&linkID=78afe03e4c68a4ea3f926228988a7b96&t=awglf-20&tracking_id=awglf-20",
+    "//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=weddingregistry&banner=13P4H6YKVYV193MANE82&f=ifr&linkID=e70a5bf375ba54e510a048636e89fe2c&t=awglf-20&tracking_id=awglf-20"
+]
+
+var randomSelectedUrl = getRandomUrl(urls);
+
+$("#adSpot").html('<iframe src=" ' + randomSelectedUrl + '" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>');
